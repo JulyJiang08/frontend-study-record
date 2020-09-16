725a9982d6c2f76f928f55df3baedf2bd5ab0964
JiangYing <904125297@qq.com>
2020-09-16 17:33:16 +0800

    commit

---
+++ Vue/ToDoList/src/components/ToDo.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="contain">
+    <div id="input" style="padding: 2px;">
+      <el-input
+      v-model="input"
+      placeholder="请输入待办事项"
+      @keyup.enter="add"
+      style="width: 100%;"
+      >       
+      </el-input>
+    </div>
+    <div id="to-do-list">
+      <div id="item" 
+      v-for="todo in todos"
+      :key="todo.index"
+      >     
+        <ToDoItem>          
+        </ToDoItem>
+      </div>
+      <div id="footer">
+        <div id="leftNum">
+
+        </div>
+        <div id="clear">
+          <el-button>
+
+          </el-button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        input:'',
+        todos:[
+          {index:1,item:"待办事项"}
+        ]
+      }
+    },
+    methods:{
+      add(){
+
+      }
+    }
+  }
+</script>
+
+<style>
+  #contain{
+    width: 35%;
+    border: 2px solid lightblue; 
+    text-align: center;
+    margin:0 auto;
+  }
+  #to-do-list{
+    width:auto;
+    border: 2px solid lightblue;
+    padding: 2px;
+  }
+</style>
